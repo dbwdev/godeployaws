25ee79ef102eeb27bf6169aa5dd756868630a9d0
Bruce Wong <dbrucewong@gmail.com>
2019-07-03 21:38:55 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"./controllers"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/lambda/deploy", controllers.DeployLambda).Methods("POST")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		fmt.Print(err)
+	}
+}
